6ed4f18e5e588ce1e864aaa11ebfd441dbbc3ce9
Tobias Weiss <tobias@tobias-weiss.org>
2021-09-01 06:59:45 +0300

    autocommit

--- python/pickle.py
+++ python/pickle.py
@@ -2,5 +2,26 @@
 
 import pickle
 
+def outline(func):
+    def inner(*args):
+        print('-'*20)
+        print(f'Function: {func.__name__')
+        func(*args)
+        print('-'*20)
+    return inner
+
 class Cat():
-    pass
+    def __init__(self, name, age, properties):
+        super().__init__()
+        self._name = name
+        self._age = age
+        self._properties = properties
+
+    @outline
+    def display(self):
+        pass
+
+oscar = Cat(name='Oscar', age=11, properties=dict(instrument='piano', sports=['soccer', 'baseball']))
+
+sc = pickle.dumps(oscar)
+print(sc)
