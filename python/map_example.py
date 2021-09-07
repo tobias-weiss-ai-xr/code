6fbd9dbe4c5471e1f3b36f0de9c0af7c8f3166d1
Tobias Weiss <tobias@tobias-weiss.org>
2021-09-07 19:06:27 +0300

    autocommit

--- python/map_example.py
+++ python/map_example.py
@@ -1,4 +1,5 @@
 # map example
+# map(func, args*)
 
 people = ['Tom', 'Maximilian', 'Hans']
 
@@ -10,3 +11,38 @@ print(f'Old way: {counts}')
 
 # modern way
 print(f'New way: {list(map(len,people))}')
+
+firstname = ('Apple', 'Chocolate', 'Fudge', 'Pizza')
+lastname = ('Pie', 'Cake', 'Brownies')
+
+def merg(a, b):
+    return a + ' ' + b
+
+x = map(merg, firstname, lastname)
+print(x)
+print(list(x))
+
+# call multiple functions in one map action
+
+def add(a, b):
+    return a+b
+
+def substract(a, b):
+    return a-b
+
+def multiply(a, b):
+    return a*b
+
+def divide(a, b):
+    return a/b
+
+def doall(func, num):
+    return func(num[0], num[1])
+
+f = (add, substract, multiply, divide)
+v = [[5, 3]]
+n = list(v) * len(f)
+print(f'f: {f}, n:{n}')
+
+m =  map(doall, f, n)
+print(list(m))
