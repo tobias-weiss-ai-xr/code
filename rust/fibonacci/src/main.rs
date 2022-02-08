6b6e72c04475a07709bc17c7c60a3b50c5cf0004
Tobias Weiss <tobias@tobias-weiss.org>
2022-02-08 09:31:49 +0100

    autocommit

---
+++ rust/fibonacci/src/main.rs
@@ -0,0 +1,33 @@
+// naive fibonacci
+
+use std::io;
+
+fn fib_first_attempt(x: u64) -> u64 {
+    if x == 0 {
+        0
+    } else if x == 1 {
+        1
+    } else {
+        fib(x - 1) + fib(x - 2)
+    }
+}
+
+fn fib(x: u64) -> u64 {
+    match x {
+        0 => 0,
+        1 => 1,
+        _ => fib(x - 1) + fib(x - 2),
+    }
+}
+
+fn main() {
+    println!("Enter n to calculate the n-th Fibonacci number.");
+
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line.");
+    let input_num: u64 = input.trim().parse().expect("Please input a number!");
+
+    println!("The Fibonacci number is: {}", fib(input_num));
+}
--- rust/more-loops/src/main.rs
+++ rust/more-loops/src/main.rs
@@ -5,4 +5,23 @@ fn main() {
         number -= 1;
     }
     println!("Liftoff!");
+
+    // another example
+    let a = [10, 20, 30, 40, 42];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("the value ist : {}", a[index]);
+        index += 1;
+    }
+
+    // another example
+    for element in a {
+        println!("the value ist : {}", element);
+    }
+
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
 }
