3a9e0853ef38a6a3b35f61ddf8f64cf3cd16a5a1
Tobias Weiss <tobias@tobias-weiss.org>
2022-08-29 07:37:19 +0200

    autocommit

---
+++ cs/StringTest/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+String s = "b\'123,123,123";
+
+s = s.Substring(2, s.Length - 3);
+
+Console.WriteLine(s);
