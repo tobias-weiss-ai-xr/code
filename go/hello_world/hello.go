e9b3ba9990c9f7cf7ce956d557fc11d616fb584f
Tobias Weiss <tobias@tobias-weiss.org>
2022-10-30 20:18:01 +0100

    autocommit

--- go/hello_world/hello.go
+++ go/hello_world/hello.go
@@ -5,6 +5,13 @@ import "fmt"
 import "rsc.io/quote"
 
 func main() {
+    var i int
+    i = 42
+    j := 42.0
+    var k float32 = 42.0
     fmt.Println("Hello, World!")
     fmt.Println(quote.Go())
+    fmt.Printf("i: value %v, type %T\n", i, i)
+    fmt.Printf("j: value %v, type %T\n", j, j)
+    fmt.Printf("i: value %v, type %T\n", k, k)
 }
