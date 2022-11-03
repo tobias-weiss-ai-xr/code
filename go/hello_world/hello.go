c8906efd13cd1579a325416491a2dac70450df33
Tobias Weiss <tobias@tobias-weiss.org>
2022-11-03 22:05:32 +0100

    autocommit

--- go/hello_world/hello.go
+++ go/hello_world/hello.go
@@ -2,12 +2,17 @@ package main
 
 import (
     "fmt"
+    "log"
     "rsc.io/quote"
     "tobias-weiss.org/greetings"
 )
 
 
 func main() {
+    // Setup logger
+    log.SetPrefix("greeting: ")
+    log.SetFlags(0)
+
     var i int
     i = 42
     j := 42.0
@@ -17,6 +22,10 @@ func main() {
     fmt.Printf("i: value %v, type %T\n", i, i)
     fmt.Printf("j: value %v, type %T\n", j, j)
     fmt.Printf("i: value %v, type %T\n", k, k)
-    message := greetings.Hello("Tobias")
+    message, err := greetings.Hello("")
+    if err != nil {
+        log.Fatal(err)
+    }
+
     fmt.Println(message)
 }
