739ad050cc80b5a2ad098cecec3c374d705eecd4
Tobias Weiss <tobias@tobias-weiss.org>
2023-11-14 07:55:41 +0100

    autocommit

--- rust/fibonacci/src/main.rs
+++ rust/fibonacci/src/main.rs
@@ -30,4 +30,6 @@ fn main() {
     let input_num: u64 = input.trim().parse().expect("Please input a number!");
 
     println!("The Fibonacci number is: {}", fib(input_num));
+
+    // println!("The slow Fibonacci number is: {}", fib_first_attempt(input_num)); 
 }
