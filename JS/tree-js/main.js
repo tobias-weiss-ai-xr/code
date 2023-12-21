525bca06fadcf0fa8c8badbc7f251e33a525c615
Tobias Weiss <tobias@tobias-weiss.org>
2023-12-21 07:55:14 +0100

    autocommit

--- JS/tree-js/main.js
+++ JS/tree-js/main.js
@@ -1,32 +1,64 @@
 import * as THREE from 'three';
 import WebGL from 'three/addons/capabilities/WebGL.js';
 
+import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
+import { FontLoader } from 'three/addons/loaders/FontLoader.js';
 
+// Scene and setup
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+// Green Box
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-//create a blue LineBasicMaterial
+// Blue Line
 const blue_material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
-
 const points = [];
 points.push( new THREE.Vector3( -1, 0, 0));
 points.push( new THREE.Vector3( 0, 3, 0));
 points.push( new THREE.Vector3( 2, 0, 0));
-
 const line_geometry = new THREE.BufferGeometry().setFromPoints( points );
-
 const line = new THREE.Line( line_geometry, blue_material);
+scene.add( line );
 
-scene.add( line )
+// Text
+// Example: https://threejs.org/examples/#webgl_geometry_text
+let textMesh1;
+const loader = new FontLoader();
+loader.load( 'fonts/helvetiker_regular.typeface.json', function ( font ) {
+
+	geometry = new TextGeometry( 'Hello three.js!', {
+		font: font,
+		size: 80,
+		height: 5,
+		curveSegments: 12,
+		bevelEnabled: true,
+		bevelThickness: 10,
+		bevelSize: 8,
+		bevelOffset: 0,
+		bevelSegments: 5
+	} );
+	geometry.computeBoundingBox();
+
+	const centerOffset = - 0.5 * ( textGeo.boundingBox.max.x - textGeo.boundingBox.min.x );
+
+	textMesh1 = new THREE.Mesh( textGeo, materials );
+
+	textMesh1.position.x = centerOffset;
+	textMesh1.position.y = hover;
+	textMesh1.position.z = 0;
+
+	textMesh1.rotation.x = 0;
+	textMesh1.rotation.y = Math.PI * 2;
+} );
+
+scene.add(textMesh1, blue_material);
 
 camera.position.z = 10;
 
